f6a2bf4d8a1f438f6b926978942fc2342903e1fc
Nicklas <Nicklas-Christensen@outlook.dk>
2024-03-01 11:44:50 +0100

    Have been trying to implement a simple chat system

--- Controllers/DataLayer.cs
+++ Controllers/DataLayer.cs
@@ -12,5 +12,18 @@ namespace PeopleHelpPeople.Controllers
     };
 
         public static IEnumerable<Match> GetAllMatches() => Matches;
+
+        //New stuff
+        public static List<Chat> Chats = new List<Chat>
+    {
+        new Chat { Id = 1, Updated = new DateTime (2015, 12, 31, 5, 10, 20), Requester = new Match { Id = 3, Name = "John Doe", Location = "New York" },
+            Provider = new Match { Id = 4, Name = "Jane Doe", Location = "Copenhagen"},
+        Messages = new List<String> {{"Hello are you getting the stuff?"}, {"Is in the store now"}, {"Sounds good!"} } } ,
+        new Chat { Id = 2, Updated = new DateTime (2024, 3, 1, 4, 20, 40), Requester = new Match { Id = 5, Name = "Amanda Young", Location = "New York" },
+            Provider = new Match { Id = 6, Name = "Michael Meyers", Location = "Copenhagen"},
+        Messages = new List<String> {{"Im allergic to strawberries"}, {"I will keep in mind"}, {"Thank you, very much!"} } }
+    };
+
+        public static IEnumerable<Chat> GetChat(int id) => Chats.Where(c => c.Id == id);
     }
 }
---
+++ Model/Chat.cs
@@ -0,0 +1,11 @@
+﻿namespace PeopleHelpPeople.Model
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public DateTime Updated { get; set; }
+        public Match Requester { get; set; }
+        public Match Provider { get; set; }
+        public List<string> Messages { get; set; }
+    }
+}
