ce9d1e7bc65e07967d5acd95feaeca14dc83715a
Mikkel Jürs <s224279@dtu.dk>
2024-04-26 22:59:08 +0200

    Fixed up user creating and duplicate code.
    Can now create users. Tested from Postman. Connecting to app next.

---
+++ Services/MongoDbUserContext.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using PHPAPI.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PHPAPI.Services
+{
+    public class MongoDbUserContext
+    {
+        private readonly IMongoDatabase _database;
+        public IMongoCollection<User> Users { get; }
+
+        public MongoDbUserContext(MongoDBSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            _database = client.GetDatabase(settings.DatabaseName);
+            Users = _database.GetCollection<User>("users");
+        }
+
+        public async Task InsertUserAsync(User user)
+        {
+            await Users.InsertOneAsync(user);
+        }
+
+        public async Task<IEnumerable<User>> FindUsersAsync(FilterDefinition<User> filter)
+        {
+            var results = await Users.FindAsync(filter);
+            return results.ToEnumerable();
+        }
+
+        public async Task<DeleteResult> DeleteUserAsync(FilterDefinition<User> filter)
+        {
+            return await Users.DeleteOneAsync(filter);
+        }
+    }
+}
